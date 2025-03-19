7267b358731cb599f7e7300b70c0306ecb335f32
Sloane <sloane@srvo.org>
2025-03-19 17:52:30 -0600

    feat: Remove utils package and add BaseScript class

--- src/dewey/utils/__init__.py
+++ src/dewey/utils/__init__.py
@@ -1,13 +1,1 @@
-"""Utility functions package for Dewey project."""
-
-from .logging import get_logger, setup_logging
-from dewey.config import load_config
-from dewey.llm.llm_utils import get_llm_client, validate_test_output
-
-__all__ = [
-    'get_logger',
-    'setup_logging',
-    'load_config',
-    'get_llm_client', 
-    'validate_test_output'
-]
+from dewey.core.base_script import BaseScript
